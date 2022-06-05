38d2ee44047f62ee061a34a0b856163e8866ba5a
dshivrin <dshivrin@gmail.com>
2022-06-05 17:04:16 +0300

    Added shared styled components

--- src/Components/AboutMe/AboutMe.tsx
+++ src/Components/AboutMe/AboutMe.tsx
@@ -1,16 +1,25 @@
 import "./AboutMe.scss";
+import styled from "styled-components";
+import { Container } from "../StyledComponents/StyledComponents";
 
 //TODO: Write a paragraph about yourself
 //Add an animated "About me" title (thinking slide from the left, large font, light color)
+
+const AboutMeContainer = styled(Container)`
+  flex-direction: row;
+  align-items: center;
+`;
+
 const AboutMe = () => {
   return (
-    <div className="container">
+    <AboutMeContainer className="about-me">
       <h1 className="title">ABOUT ME</h1>
       <p>
-        I'm a full-stack web developer based in Israel. Professionally I code since 2015. I'm an autodidact person,
-        experienced with variaty of technologies and approaches. 
+        I'm a full-stack web developer based in Israel. Professionally I code
+        since 2015. I'm an autodidact person, experienced with variaty of
+        technologies and approaches.
       </p>
-    </div>
+    </AboutMeContainer>
   );
 };
 
---
+++ src/Components/StyledComponents/StyledComponents.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  justify-items: center;
+  text-align: center;
+  flex-direction: column;
+`;
