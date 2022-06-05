7a8add90e797f2516de04d94b83c39531b579257
dshivrin <dshivrin@gmail.com>
2022-06-05 13:44:10 +0300

    AboutMe context added

--- src/Components/AboutMe/AboutMe.tsx
+++ src/Components/AboutMe/AboutMe.tsx
@@ -1,18 +1,17 @@
-
 import "./AboutMe.scss";
 
 //TODO: Write a paragraph about yourself
-//Add an animated "About me" title (thinking slide from the left, large font, light color) 
-const AboutMe = () =>{
+//Add an animated "About me" title (thinking slide from the left, large font, light color)
+const AboutMe = () => {
   return (
     <div className="container">
-      <h1>ABOUT ME</h1>
+      <h1 className="title">ABOUT ME</h1>
       <p>
-       I'm a full-stack web developer since 2015. I'm an autodidact person. 
+        I'm a full-stack web developer based in Israel. Professionally I code since 2015. I'm an autodidact person,
+        experienced with variaty of technologies and approaches. 
       </p>
-      
     </div>
   );
-}
+};
 
 export default AboutMe;
