647fea12c02ab74692f87f5d186686ba27398251
dshivrin <dshivrin@gmail.com>
2022-06-08 16:49:34 +0300

    Experience - basic functionality

--- src/Components/Experience/Experience.tsx
+++ src/Components/Experience/Experience.tsx
@@ -49,11 +49,7 @@ const Experience = () => {
               </button>
             </li>
             <li>
-              <button
-                id="Prior Experience"
-                type="button"
-                onClick={OnTitleClick}
-              >
+              <button id="Prior" type="button" onClick={OnTitleClick}>
                 Prior Experience
               </button>
             </li>
@@ -61,14 +57,19 @@ const Experience = () => {
         </div>
         <div className="content-container">
           <p id="section-Smoove" style={{ display: "block" }}>
-            <span className="job-title">Junior Full Stack developer</span>. The
-            journey begins! Specialized on .NET framework, microservices
-            architechture and MSSQL on the back-end. Used AngularJS and Angular
-            2, JQuery, Material UI on the front-end. Developed event driven
-            applications in the Human Resources wold.
-            <span>Jan 2015 - Feb 2018</span>
+            <span className="job-title">Senior Full Stack developer.</span>{" "}
+            Moved to my next challange and joined an amazing company in the
+            Marketing and Automation world. This time mastering ReactJS, and
+            <span>Feb 2020 - May 2022</span>
           </p>
           <p id="section-Aeronautics" style={{ display: "none" }}>
+            <span className="job-title">C# Software developer.</span> I took a
+            chalange outside of web-development world with a defence contractor.
+            Maintained an AngularJS generator application and took part in
+            developing the company's next flight simulator.
+            <p>August 2018 - Jan 2020</p>
+          </p>
+          <p id="section-Sagramatha" style={{ display: "none" }}>
             <span className="job-title">MEAN Stack developer.</span> I gladly
             took the opportunity to go all-in with JavaScript! Node.js,
             express.js, Angular 2+, MongoDB and MSSQL. With this well
@@ -76,24 +77,24 @@ const Experience = () => {
             system for large retailers mainly in the U.S.
             <span>March 2018 - August 2018</span>
           </p>
-          <p id="section-Sagramatha" style={{ display: "none" }}>
-            <span className="job-title">C# Software developer.</span> I took a
-            chalange outside of web-development world with a defence contractor.
-            Maintained an AngularJS generator application and took part in
-            developing the company's next flight simulator.
-            <p>August 2018 - Jan 2020</p>
-          </p>
           <p id="section-Niloosoft" style={{ display: "none" }}>
-            <span className="job-title">Senior Full Stack developer.</span>{" "}
-            Moved to my next challange and joined an amazing company in the
-            Marketing and Automation world. This time mastering ReactJS, and
-            <span>Feb 2020 - May 2022</span>
+            <span className="job-title">Junior Full Stack developer</span>. The
+            journey begins! Specialized on .NET framework, microservices
+            architechture and MSSQL on the back-end. Used AngularJS and Angular
+            2, JQuery, Material UI on the front-end. Developed event driven
+            applications in the Human Resources wold.
+            <span>Jan 2015 - Feb 2018</span>
           </p>
           <p id="section-Prior Experience" style={{ display: "none" }}>
-            <span className="job-title">Senior Full Stack developer.</span>{" "}
-            Moved to my next challange and joined an amazing company in the
-            Marketing and Automation world. This time mastering ReactJS, and
-            <span>Feb 2020 - May 2022</span>
+            Prior to software development I was the best tech support engineer
+            you can find. I have worked in local ISP support center, soon to
+            become a senior support engineer, then shift manager and evetually
+            tech support team lead.
+          </p>
+          <br />
+          <p id="section-Prior Experience2" style={{ display: "none" }}>
+            As a result I have a wide understanding of networking, routing and
+            managment skills.
           </p>
         </div>
       </div>
