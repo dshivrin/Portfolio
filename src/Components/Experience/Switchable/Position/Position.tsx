f27fa8c5c2e19961e7e8f535917a1de5a175c546
dshivrin <dshivrin@gmail.com>
2022-06-16 15:50:15 +0300

    Some fixes

--- src/Components/Experience/Switchable/Position/Position.tsx
+++ src/Components/Experience/Switchable/Position/Position.tsx
@@ -3,7 +3,7 @@ import { PositionProps } from "Core/types";
 const Position = (props: PositionProps) => {
   const { workplace } = props;
   return (
-    <h1>
+    <h1 className="position">
       {workplace.position} at <a href={workplace.website}>{workplace.name}</a>
     </h1>
   );
