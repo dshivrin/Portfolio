67170fdfe06156e427bcee33de3992192d95ca85
dshivrin <dshivrin@gmail.com>
2022-07-04 14:18:46 +0300

    Intro changes

--- src/Components/Intro/Intro.tsx
+++ src/Components/Intro/Intro.tsx
@@ -1,5 +1,7 @@
+import AnimatedText from "Components/Shared/AnimatedTyping/AnimatedTyping";
 import styled from "styled-components";
 import { Container } from "../Shared/StyledComponents/Container";
+import "./Intro.scss";
 
 const Name = styled.h1`
   font-family: Madelican;
@@ -17,27 +19,30 @@ const Header = styled.div`
 `;
 
 const IntroContainer = styled(Container)`
-  name {
-    font-family: Madelican;
-    font-size: clamp(
-      1.5em,
-      12vw,
-      4.5em
-    ); //clamp(min, value, maximum), base value is set in
-  }
-  header {
-    justify-items: start;
-  }
+  text-align: left;
+  width: 800px;
+  margin: 0 auto;
 `;
+
+const animatedTextData = {
+  element: "h1",
+  text: ["Test hello world"],
+  typeDelay: 200,
+  eraseDelay: 100,
+  nextWordDelay: 1000,
+  iterations: 1,
+};
+
 const Intro = () => {
   return (
     <IntroContainer id="intro">
       <Header>
-        <br />
-        <span>Hi there! I am</span>
+      {/* <AnimatedText {...animatedTextData} /> */}
+        <h4 className="intro">Hi there! I am</h4>
       </Header>
-      <Name className="name">Dima Shivrin</Name>
-      <div>I am a senior Full-Stack Engineer</div>
+      {/* <Name className="name">Dima Shivrin</Name> */}
+      <h3 className="name">Dima Shivrin.</h3>
+      <h3 className="description">I'm a Senior Full-Stack Engineer</h3>
     </IntroContainer>
   );
 };
