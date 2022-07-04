a76ae214890bef5ee866e8e7777a7b9babb91d08
dshivrin <dshivrin@gmail.com>
2022-07-04 17:19:35 +0300

    Button link animations

--- src/Components/Shared/ButtonLink/ButtonLink.tsx
+++ src/Components/Shared/ButtonLink/ButtonLink.tsx
@@ -1,16 +1,26 @@
-import './ButtonLink.scss';
+import "./ButtonLink.scss";
 
 //this must be a div due to animations.
 const ButtonLink = (props: any) => {
-   
   const navigate = (url: string) => {
-    window.open(url, '_blank', 'noopener,noreferrer');
+    window.open(url, "_blank", "noopener,noreferrer");
   };
 
   return (
     <div onClick={() => navigate(props.website)} className="box">
       <div className="box-inner">
         <h2>{props.name}</h2>
+        <div id="arrowAnim">
+          <div className="arrowSliding">
+            <div className="arrow"></div>
+          </div>
+          <div className="arrowSliding delay1">
+            <div className="arrow"></div>
+          </div>
+          <div className="arrowSliding delay2">
+            <div className="arrow"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
