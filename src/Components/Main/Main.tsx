2d0772eaab3fcb671610a6585978a56ad021a844
dshivrin <dshivrin@gmail.com>
2022-07-13 13:49:00 +0300

    Some cleanup

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -3,9 +3,7 @@ import AboutThisApp from "Components/AboutThisApp/AboutThisApp";
 import Contact from "Components/Contact/Contact";
 import Experience from "Components/Experience/Experience";
 import Footer from "Components/Footer/Footer";
-import Hobbies from "Components/Hobbies/Hobbies";
 import Intro from "Components/Intro/Intro";
-import Projects from "Components/Projects/Projects";
 
 
 const Main = () => {
@@ -14,8 +12,6 @@ const Main = () => {
       <Intro />
       <AboutMe />
       <Experience />
-      {/* <Projects />
-      <Hobbies /> */}
       <AboutThisApp />
       <Contact />
       <Footer />
