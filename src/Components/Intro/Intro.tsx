7af8709cbc3e6cd079be6c55f3ccd4a31665928a
dshivrin <dshivrin@gmail.com>
2022-07-17 10:08:43 +0300

    Intro responsive fix

--- src/Components/Intro/Intro.tsx
+++ src/Components/Intro/Intro.tsx
@@ -1,6 +1,7 @@
 import AnimatedText from "Components/Shared/AnimatedTyping/AnimatedTyping";
 import styled from "styled-components";
 import { Container } from "../Shared/StyledComponents/Container";
+import { StyledInner } from "../Shared/StyledComponents/InnerContainer";
 import "./Intro.scss";
 
 const Name = styled.h1`
@@ -22,6 +23,9 @@ const IntroContainer = styled(Container)`
   text-align: left;
   width: 800px;
   margin: 0 auto;
+  /* @media (max-width: 768px) {
+    margin: 0 auto;
+  } */
 `;
 
 const animatedTextData = {
@@ -36,13 +40,17 @@ const animatedTextData = {
 const Intro = () => {
   return (
     <IntroContainer id="intro">
-      <Header>
-      {/* <AnimatedText {...animatedTextData} /> */}
-        <h4 className="intro">Hi there! I am</h4>
-      </Header>
-      {/* <Name className="name">Dima Shivrin</Name> */}
-      <h3 className="name">Dima Shivrin.</h3>
-      <h3 className="description">A Senior Full-Stack Engineer.</h3>
+      <StyledInner>
+        <div>
+          <Header>
+            {/* <AnimatedText {...animatedTextData} /> */}
+            <h4 className="intro">Hi there! I am</h4>
+          </Header>
+          {/* <Name className="name">Dima Shivrin</Name> */}
+          <h3 className="name">Dima Shivrin.</h3>
+          <h3 className="description">A Senior Full-Stack Engineer.</h3>
+        </div>
+      </StyledInner>
     </IntroContainer>
   );
 };
