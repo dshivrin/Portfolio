957cd54d8fdc3e1ddb1dff4eb0ef8e1eeda97c36
dshivrin <dshivrin@gmail.com>
2022-09-01 11:27:10 +0300

    Experience title CSS changes

--- src/Components/Experience/Switchable/Position/Position.tsx
+++ src/Components/Experience/Switchable/Position/Position.tsx
@@ -1,6 +1,8 @@
-import ButtonLink from "Components/Shared/ButtonLink/ButtonLink";
+// import ButtonLink from "Components/Shared/ButtonLink/ButtonLink";
+import Link from "Components/Shared/Link/Link";
 import { TitleContainer } from "Components/Shared/StyledComponents/Container";
 import { PositionProps } from "Core/types";
+import "./Position.scss"
 
 const Position = (props: PositionProps) => {
   const { workplace, styles } = props;
@@ -9,7 +11,12 @@ const Position = (props: PositionProps) => {
   return (
     <TitleContainer style={{...styles}}>
       <h1 className="position">{workplace.position} at</h1>
-      <ButtonLink {...workplace} />
+      {/* <ButtonLink {...workplace} /> */}
+      <Link
+                  cssclass="link company-link"
+                  href={workplace.website}
+                  text={workplace.name}
+                />
     </TitleContainer>
   );
 };
--- src/Components/Shared/StyledComponents/Anchor.ts
+++ src/Components/Shared/StyledComponents/Anchor.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const StyledAnchor = styled.a`
 text-decoration: none;
 color: var(--light-green);
-font-size: 1em;
+//font-size: 1em;
 font-weight: 200;
 &:hover{
     color: var(--font-color);
